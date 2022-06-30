e9a5d52915b75f336109b2e912119082e8e05b23
Matt Schwartz <19769905+schwartzmj@users.noreply.github.com>
2022-06-30 12:05:44 -0700

    add radial gradients

--- tailwind.config.site.js
+++ tailwind.config.site.js
@@ -18,6 +18,17 @@ module.exports = {
         body: ["Open Sans", "sans-serif"],
         heading: ["Roboto Slab", "serif"],
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial-to-tr":
+          "radial-gradient(115% 90% at 0% 100%, var(--tw-gradient-stops))",
+        "gradient-radial-to-tl":
+          "radial-gradient(115% 90% at 100% 100%, var(--tw-gradient-stops))",
+        "gradient-radial-to-br":
+          "radial-gradient(90% 115% at 0% 0%, var(--tw-gradient-stops))",
+        "gradient-radial-to-bl":
+          "radial-gradient(90% 115% at 100% 0%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [
