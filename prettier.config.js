0f65bb9bbd4d9143329673f19b357e85877817dd
Matt Schwartz <19769905+schwartzmj@users.noreply.github.com>
2022-07-18 06:59:20 -0700

    add new antlers prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    tailwindConfig: './tailwind.config.site.js',
-  }
+  tailwindConfig: "./tailwind.config.site.js",
+  plugins: ["./node_modules/prettier-plugin-antlers/"],
+  overrides: [
+    {
+      files: "*.antlers.html",
+      vscodeLanguageIds: ["antlers"],
+      options: {
+        parser: "antlers",
+      },
+    },
+  ],
+};
