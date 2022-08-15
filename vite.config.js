ddfdf6fa161f0380f27a5146d771e31c38ca15eb
Matt Schwartz <19769905+schwartzmj@users.noreply.github.com>
2022-08-15 09:45:29 -0700

    svelte -> preact

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preact from "@preact/preset-vite";
 // import vue from '@vitejs/plugin-vue';
 // Partytown
 import path from "path";
@@ -28,14 +28,12 @@ export default defineConfig({
       "resources/js/app.js",
       // Components
       "resources/js/glightbox.js",
-      "resources/js/docSearch/render.ts",
+      "resources/js/Preact/Counter/render.tsx",
       // CP
       "resources/css/cp.css",
       "resources/js/cp.js",
     ]),
-    svelte({
-      /* plugin options */
-    }),
+    preact(),
     // laravel({
     //   input: "resources/js/app.js",
     //   publicDirectory: "public", // default
